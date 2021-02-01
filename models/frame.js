2108fd3c9b0742b784d4abf85fdd6e04a1e8e2bd
Nick Elliott <nickmartinelliott@gmail.com>
2021-02-01 02:22:12 -0500

    Change the alias in product-frame association from hardware to frames.

--- models/frame.js
+++ models/frame.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Frame.belongsTo(models.Product, { 
-        as: 'hardware',
+        as: 'frames',
         foreignKey: {
           fieldName: 'productId',
           type: DataTypes.INTEGER,
